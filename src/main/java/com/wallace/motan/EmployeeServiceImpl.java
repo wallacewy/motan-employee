2032148942baf4e8787fe059944e288dd60fed6b
Wallace Wan <wallace.wan@sap.com>
2016-11-04 10:11:52 +0800

    Initial project

---
+++ src/main/java/com/wallace/motan/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.wallace.motan;
+
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	public String hello(String name) {
+        System.out.println(name + " invoked employee service");
+        return "Hello " + name + ", it's greeting from employee service";
+    }
+}
