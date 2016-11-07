80a9c8dcdb649e7d2d021924492758257ed1d2d7
Wallace Wan <wallace.wan@sap.com>
2016-11-07 16:43:04 +0800

    Output: group v1

--- src/main/java/com/wallace/motan/EmployeeServiceImpl.java
+++ src/main/java/com/wallace/motan/EmployeeServiceImpl.java
@@ -4,6 +4,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 	
 	public String hello(String name) {
         System.out.println(name + " invoked employee service");
-        return "Hello " + name + ", it's greeting from employee service";
+        return "Hello " + name + ", it's greeting from employee service (group v1)";
     }
 }
